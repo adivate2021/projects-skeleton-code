3f7452583f3bedece9ce89bf40ecf7c8256e72af
Jinyuan Zhang <martinzhang0314@163.com>
2023-04-24 19:37:54 -0700

    latest update

--- data/StartingDataset.py
+++ data/StartingDataset.py
@@ -13,33 +13,24 @@ class StartingDataset(torch.utils.data.Dataset):
     Dataset that contains 100000 3x224x224 black images (all zeros).
     """
 
-    def __init__(self, image_ids, labels):
-        pd.read_csv("/cassava-leaf-disease-classification/train.csv")
+    def __init__(self):
+        data_csv = pd.read_csv("/cassava-leaf-disease-classification/train.csv")
         
-        self.image_ids = image_ids
-        self.labels = labels
-
-        
-        # inputs = torch.zeros([3, 224, 224])
-        #label = 0
-        # convert_tensor = transforms.ToTensor()
-        # path_image = "~/Desktop/cassava-leaf-disease-classification/train_images"
-        # inputs = []
-        # for img in os.path(path_image):
-        #     img_array = cv2.imread(os.path.join(path_image, img))
-        #     new_array = cv2.resize(img_array, (224, 224))
-        #     b,g,r = cv2.split(new_array)
-        #     inputs.append([b,g,r])
-        # print(inputs[0])
+        self.image_ids = data_csv["image_id"]
+        self.labels = data_csv["label"]
 
 
 
     def __getitem__(self, index):
+        path_image = "~/Desktop/cassava-leaf-disease-classification/train_images" #path to the training image folder
+
         image_id = self.image_ids[index]
         label = self.labels[index]
         both = (image_id, label)
-        
-        return both
+        image = cv2.imread(os.path.join(path_image, image_id))
+        image_array = torch.Tensor(image)
+
+        return image_array
 
     def __len__(self):
         return len(self.image_ids)
