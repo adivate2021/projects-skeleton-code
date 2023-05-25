0d1cc3dddcd1784447993da1bbbd5b171e54827b
adivate2021 <44125685+adivate2021@users.noreply.github.com>
2023-05-24 18:38:27 -0700

    Merged Martins branch with starting dataset and starting train changes

--- data/StartingDataset.py
+++ data/StartingDataset.py
@@ -30,3 +30,27 @@ class StartingDataset(torch.utils.data.Dataset):
 
     def __len__(self):
         return len(self.image_ids)
+
+'''
+import torch
+import pandas as pd
+
+class StartingDataset(torch.utils.data.Dataset):
+    """
+    Dataset that contains 100000 3x224x224 black images (all zeros).
+    """
+
+    def __init__(self, image_ids, labels):
+        self.image_ids = image_ids
+        self.labels = labels
+
+    def __getitem__(self, index):
+        image_id = self.image_ids[index]
+        label = self.labels[index]
+        both = (image_id, label)
+
+        return both
+
+    def __len__(self):
+        return len(self.image_ids)
+'''
